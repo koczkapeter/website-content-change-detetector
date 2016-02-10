479f2e617cd29962f08d3bbfe090545482abb92d
Koczka Péter <>
2016-02-10 23:44:30 +0100

    First Commit

---
+++ src/website/content/change/detetector/WebsiteContentChangeDetetector.java
@@ -0,0 +1,13 @@
+package website.content.change.detetector;
+
+/**
+ *
+ * @author koczka.peter
+ */
+public class WebsiteContentChangeDetetector {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
